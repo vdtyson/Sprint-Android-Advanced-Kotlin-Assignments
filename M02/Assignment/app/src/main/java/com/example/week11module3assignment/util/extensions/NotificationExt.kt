8964d7e7880ba3d8bd481b438e0cedccbbe38c84
vdtyson <versilistyson@gmail.com>
2019-10-16 18:25:16 -0400

    Notifications

--- M02/Assignment/app/src/main/java/com/example/week11module3assignment/util/extensions/NotificationExt.kt
+++ M02/Assignment/app/src/main/java/com/example/week11module3assignment/util/extensions/NotificationExt.kt
@@ -8,29 +8,37 @@ import android.os.Build.*
 import android.os.Build.VERSION.*
 import android.provider.Settings.Global.getString
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat.getSystemService
 import com.example.week11module3assignment.R
+import com.example.week11module3assignment.util.extensions.NotificationStuff.CHANNEL_ID
+import com.example.week11module3assignment.util.extensions.NotificationStuff.NOTIFICATION_ID
 
-fun Context.showNotification(channelId: String, icon: Int, title: String = "", description: String) {
+object NotificationStuff {
+    const val CHANNEL_ID = "CHANNEL_ID"
+    const val NOTIFICATION_ID = 0
+}
+fun Context.createNotification(channelId: String, icon: Int, title: String = "", description: String) {
     NotificationCompat.Builder(this,channelId)
         .setSmallIcon(icon)
         .setContentTitle(title)
         .setContentText(description)
         .priority = NotificationCompat.PRIORITY_DEFAULT
 }
-private fun createNotificationChannel() {
-    // Create the NotificationChannel, but only on API 26+ because
-    // the NotificationChannel class is new and not in the support library
-    if (Build.SDK_INT >= VERSION_CODES.O) {
-        val name = getString(R.string.channel_name)
-        val descriptionText = getString(R.string.channel_description)
+fun Context.createNofificationChannel(descriptionText: String) {
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = NotificationStuff.CHANNEL_ID
         val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+        val channel = NotificationChannel(CHANNEL_ID,name,importance). apply {
             description = descriptionText
         }
-        // Register the channel with the system
         val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
+    }
+}
+fun Context.showNotification() {
+    with(NotificationManagerCompat.from(this)) {
+        notify(NOTIFICATION_ID, builder.Build())
     }
 }
